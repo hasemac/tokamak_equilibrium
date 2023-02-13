14dd7860df05dd60371b61cff0130c7ba042f9f7
machase <hasegawa@triam.kyushu-u.ac.jp>
2023-02-13 12:57:07 +0900

    trim tf rewind

--- sub/functions.py
+++ sub/functions.py
@@ -613,10 +613,8 @@ def equi_pre_process(condition, verbose=2):
     dm['matrix'] = np.ones((g.nz, g.nr))*cond["cur_tf"]["tf"]*cond["cur_tf"]["turn"]
     cond['pol_current'] = dm
     
-    # TFコイルの巻き戻し
-    if 'rewind' in cond['cur_tf']:
-        if cond['cur_tf']['rewind']:
-            cond['cur_pf']['tf_rewind'] = cond['cur_tf']['tf']
+    # TFコイル巻き戻しのチェック
+    cond = ssf.check_tf_rewind(cond)
     
     # コイルによるフラックス
     dm_fc = cmat.get_flux_of_coil(cond)
